f4972a4532b6dbe82f0ddaad41ebc8124e700a73
milkyklim <klim.kolyvanov@gmail.com>
2019-07-11 21:37:56 +0200

    add script to parse data from infura and read data from file

--- cheeze-wizard/scripts/hello_eth.py
+++ cheeze-wizard/scripts/hello_eth.py
@@ -3,9 +3,15 @@
 '''
 
 import json
+import pickle
 import requests
 from web3.auto.infura import w3
+# from tqdm import tqdm
 
+# this info is coming from etherscan
+# https://etherscan.io/address/0x2f4bdafb22bd92aa7b7552d270376de8edccbc1e
+CW_ADDRESS = '0x2F4Bdafb22bd92AA7b7552d270376dE8eDccbc1E'
+CW_BLOCK = 7779141
 
 def get_contract_abi(address):
     '''
@@ -22,12 +28,21 @@ def get_contract_abi(address):
         return None
 
 
-print(f'Current block: {w3.eth.blockNumber}')
-print(f'Current gas price: {w3.eth.gasPrice}')
+if __name__ == "__main__":
+    abi = get_contract_abi(CW_ADDRESS)
+    print('got abi')
+    contract = w3.eth.contract(CW_ADDRESS, abi=abi)
+    print('got contract')
 
-CW = '0x2F4Bdafb22bd92AA7b7552d270376dE8eDccbc1E'
-
-print(f'Cheeze wizards balance: {w3.eth.getBalance(CW)}')
-
-abi = get_contract_abi(CW)
-contract = w3.eth.contract(CW, abi=abi)
+    # this works on the local node
+    # logs = contract.events.WizardSummoned().getLogs(fromBlock=CW_BLOCK)
+    # this one works with infura
+    # logs = []
+    # for start in tqdm(range(CW_BLOCK, w3.eth.blockNumber, 1000)):
+    #     end = min(start + 1000, w3.eth.blockNumber)
+    #     logs.extend(contract.events.WizardSummoned().getLogs(fromBlock=start, toBlock=end))
+    # this one for loading data
+    logs = pickle.load(open('wiz.pickle', 'rb'))
+    
+    for k in logs:
+        print(k['args']['element'], k['args']['power'])
