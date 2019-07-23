5e1481b36abe96d9a8ac56a8d2b753248ea37ee5
milkyklim <klim.kolyvanov@gmail.com>
2019-07-23 22:25:47 +0200

    add script that saves wizard ids to csv and returns total number of elementals

--- cheeze-wizard/scripts/save_wizard_ids.py
+++ cheeze-wizard/scripts/save_wizard_ids.py
@@ -1,5 +1,5 @@
 '''
-    function
+    function to save all token ids to dataframe
 '''
 
 import json
@@ -7,18 +7,28 @@ import os
 import pickle
 import requests
 from web3.auto.infura import w3
+import pandas as pd
 from tqdm import tqdm
 
 # this info is coming from etherscan
 # https://etherscan.io/address/0x2f4bdafb22bd92aa7b7552d270376de8edccbc1e
 CW_ADDRESS = '0x2F4Bdafb22bd92AA7b7552d270376dE8eDccbc1E'
 CW_BLOCK = 7779141
+
+ELEMENT_WIZARD = {
+    1 : 'ELEMENT_NEUTRAL',
+    2 : 'ELEMENT_FIRE',
+    3 : 'ELEMENT_WIND',
+    4 : 'ELEMENT_WATER',
+}
+MAX_ELEMENT = 4
+
 DATAPATH = '../data'
 
-def get_contract(address):
-    '''Get contract interface and cache it.'''
+def get_contract(address, overwrite=False):
+    '''Get contract interface and cache it'''
     filepath = os.path.join(DATAPATH, 'input', 'contract', f'{address}.json')
-    if not os.path.exists(filepath):
+    if overwrite or not os.path.exists(filepath):
         # cache the response
         abi = get_contract_abi(address)
         with open(filepath, 'w', encoding='utf-8') as f:
@@ -27,7 +37,6 @@ def get_contract(address):
         abi = json.load(f)
     return w3.eth.contract(address, abi=abi)
 
-# TODO: add caching, too
 def get_contract_abi(address):
     '''Get contract interface from Etherscan'''
     resp = requests.get(
@@ -40,23 +49,54 @@ def get_contract_abi(address):
     except json.JSONDecodeError:
         return None
 
-def get_wizards(contract):
-    '''Get all summoned wizards.'''
+def get_wizards(contract, overwrite=False):
+    '''Get all summoned wizards'''
     w_filename = 'wizards.pickle'
     filepath = os.path.join(DATAPATH, 'input', w_filename)
-    if not os.path.exists(filepath):
+    if overwrite or not os.path.exists(filepath):
         # works on local node
         # wizards = contract.events.WizardSummoned().getLogs(fromBlock=CW_BLOCK)
         # works on infura
         wizards = []
-        for start in tqdm(range(CW_BLOCK, w3.eth.blockNumber, 1000)):
-            end = min(start + 1000, w3.eth.blockNumber)
-            wizards.extend(contract.events.WizardSummoned().getLogs(fromBlock=start, toBlock=end))
+        step = 1000
+        for start in tqdm(range(CW_BLOCK, w3.eth.blockNumber, step)):
+            end = min(start + step - 1, w3.eth.blockNumber)
+            wizards.extend(
+                contract.events.WizardSummoned().getLogs(
+                    fromBlock=start, 
+                    toBlock=end,
+                )
+            )
         with open(filepath, 'ab') as f:
             pickle.dump(wizards, f)                      
     wizards = pickle.load(open(filepath, 'rb'))
     return wizards
 
+def get_counts(wizards):
+    '''Count number of elements'''
+    total = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+    }
+
+    for w in wizards:
+        element = w['args']['element']
+        total[element] += 1
+        
+    for i in range(1, MAX_ELEMENT + 1):
+        print(f'{ELEMENT_WIZARD[i]}: {total[i]}')  
+
+    print(f'TOTAL: {sum(total.values())}')
+
+def get_all_ids(wizards):
+    df = pd.DataFrame(
+        data={
+            'tokenId': [w['args']['tokenId'] for w in wizards], 
+        },
+    )
+    return df
 
 
 if __name__ == "__main__":
@@ -64,3 +104,11 @@ if __name__ == "__main__":
     print('got contract')
     wizards = get_wizards(contract)
     print('got wizards')
+
+    get_counts(wizards)
+    df_ids = get_all_ids(wizards)
+
+    df_ids.sort_values(by=['tokenId']).to_csv(
+        '../data/output/token_id.csv',
+        index=False,
+    )
